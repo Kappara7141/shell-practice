99fc3af5c5954e921fc2673f8cea31cec8da9d4c
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-10 16:00:54 +0530

    some message

--- variables.sh
+++ variables.sh
@@ -4,6 +4,6 @@ PERSON1=Sachin
 PERSON2=Rahul
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
-echo "$PERSON2:: Hello PERSON1, Iam fine. How are you?"
+echo "$PERSON2:: Hello $PERSON1, Iam fine. How are you?"
 echo "$PERSON1:: What are you learning recently"
 echo "$PERSON2:: I am learning shell scripting $PERSON1"
