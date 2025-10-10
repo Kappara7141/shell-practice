fb164610848ed0007dc6349f8e5faadef1b4b9a6
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-10 15:01:26 +0530

    some message

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, Iam fine. How are you?"
+echo "Ramesh:: What are you learning recently"
+echo "Suresh:: I am learning shell scripting Ramesh"
