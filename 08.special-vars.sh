53665df835f47498e732af4f20f2fd01a1499006
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-15 16:10:15 +0530

    some message

--- 08.special-vars.sh
+++ 08.special-vars.sh
@@ -8,3 +8,7 @@ echo "Current working directory: $PWD"
 echo "Who is running this: $USER"
 echo "Home directory of user: $HOME"
 echo "PID of this script: $$"
+
+sleep 10 &
+
+echo "PID of the last command in background is: $!"
