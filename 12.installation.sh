c7bbd3938cda14183b6449ca385ccce3a9e0a9f0
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-16 22:27:35 +0530

    some message

--- 12.installation.sh
+++ 12.installation.sh
@@ -7,20 +7,11 @@ if [ $USERID -ne 0 ]; then
     exit 1 #failure is other than 0
 fi
 
-VALIDATE(){ #functions receive inputs through args just like shell script args
-    if [ $1 -ne 0 ]; then
-        echo "ERROR:: Installating $2 is FAILURE"
-        exit 1
-    else
-        echo "Installating $2 is SUCCESS"   
-    fi     
-}
-
 dnf install mysql -y
-VALIDATE $? "MySQL"
 
-dnf install nginx -y
-VALIDATE $? "Nginx"
-
-dnf install mongodb-mongosh -y
-VALIDATE $? "mongosh"
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installating MySQL is FAILURE"
+    exit 1
+else
+    echo "Installating MySQL is SUCCESS"   
+fi     
