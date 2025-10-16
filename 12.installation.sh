607802b26bcd34186fd28bb1a644f63c0e895b7f
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-16 17:24:24 +0530

    some message

--- 12.installation.sh
+++ 12.installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root privilege"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Installating MySQL is FAILURE"
+    exit 1
 else
     echo "Installating MySQL is SUCCESS"   
 fi     
