88bb734a5395a75abcd74879a8d5835323287b38
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-16 22:12:56 +0530

    some message

--- 12.installation.sh
+++ 12.installation.sh
@@ -4,14 +4,23 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root privilege"
-    exit 1
+    exit 1 #failure is other than 0
 fi
 
-dnf install mysql -y
+VALIDATE(){ #functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installating $2 is FAILURE"
+        exit 1
+    else
+        echo "Installating $2 is SUCCESS"   
+    fi     
+}
 
-if [ $? -ne 0 ]; then
-    echo "ERROR:: Installating MySQL is FAILURE"
-    exit 1
-else
-    echo "Installating MySQL is SUCCESS"   
-fi     
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
