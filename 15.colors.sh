1f2910cce3fe30adef11cbcaa5b23a9ea84b97b8
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-18 15:22:50 +0530

    some message

--- 15.colors.sh
+++ 15.colors.sh
@@ -14,10 +14,10 @@ fi
 
 VALIDATE(){ #function receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo "ERROR:: Installating $2 is $R FAILURE"
+        echo -e "ERROR:: Installating $2 is $R FAILURE"
         exit 1
     else
-        echo "Installating $2 is $G SUCCESS"   
+        echo -e "Installating $2 is $G SUCCESS"   
     fi     
 }
 dnf list installed mysql
@@ -25,7 +25,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $? "MySQL"
 else 
-    echo "MySQL already exist ... $G skipped $N"
+    echo -e "MySQL already exist ... $G skipped $N"
 fi
 
 dnf list installed nginx
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? "Nginx"
 else 
-    echo "Nginx already exist ... $G skipped $N"
+    echo -e "Nginx already exist ... $G skipped $N"
 fi
 
 dnf list installed python3
@@ -41,5 +41,5 @@ if [ $? -ne 0 ]; then
     dnf install python3 -y
     VALIDATE $? "python3"
 else
-    echo "Python3 already exist ... $G skipped $N"
+    echo -e "Python3 already exist ... $G skipped $N"
 fi
