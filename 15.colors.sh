9bb3179e23f59078b42813162d98dbc42dedc34e
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-18 15:24:05 +0530

    some message

--- 15.colors.sh
+++ 15.colors.sh
@@ -25,7 +25,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $? "MySQL"
 else 
-    echo -e "MySQL already exist ... $G skipped $N"
+    echo -e "MySQL already exist ... $Y skipped $N"
 fi
 
 dnf list installed nginx
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? "Nginx"
 else 
-    echo -e "Nginx already exist ... $G skipped $N"
+    echo -e "Nginx already exist ... $Y skipped $N"
 fi
 
 dnf list installed python3
@@ -41,5 +41,5 @@ if [ $? -ne 0 ]; then
     dnf install python3 -y
     VALIDATE $? "python3"
 else
-    echo -e "Python3 already exist ... $G skipped $N"
+    echo -e "Python3 already exist ... $Y skipped $N"
 fi
