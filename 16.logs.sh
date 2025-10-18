d0887e89e927f65f684352d18b83cab4633562fa
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-18 17:01:09 +0530

    some message

--- 16.logs.sh
+++ 16.logs.sh
@@ -8,8 +8,8 @@ Y="\e[33m"
 N="\e[30m"
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root privilege"
