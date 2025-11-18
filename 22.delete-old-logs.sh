b6bc74c7b77e82750f9d6bcd8683e1760f76509d
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-11-18 15:12:16 +0530

    some message

---
+++ 22.delete-old-logs.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[30m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at : $(date)" | tee -a $LOG_FILE
+
+SOURCE_DIR=/home/ec2-user/app-logs/
+
+if [! -d $SOURCE_DIR];then
+    echo "ERROR :: $SOURCE_DIR doesn't exist"
+    exit 1
+fi    
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+
