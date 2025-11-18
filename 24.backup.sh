825dcd0aea55a1799f3ebb2268718a13e18af28a
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-11-18 23:11:21 +0530

    some message

--- 24.backup.sh
+++ 24.backup.sh
@@ -10,7 +10,7 @@ N="\e[30m"
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-
+DAYS=${3:- 14};  #if not provided considered as 14 days
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at : $(date)" | tee -a $LOG_FILE
 
@@ -19,7 +19,7 @@ if [ $USERID -ne 0 ]; then
     exit 1 #failure is other than 0
 fi
 
-usage(){
+USAGE(){
     echo -e "$R USAGE:: sudo sh 24.backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days]$N"
     exit 1
 }
@@ -36,6 +36,14 @@ fi
 if [ ! -d DEST_DIR ]; then
     echo -e "$R DEST_DIR doesn't exist $N"
     exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+
+if [ ! -z "${FILES}" ]; then
+    echo "Files found"
+else
+    echo "No files to archive $Y SKIPPING... $N"
 fi
 
 
