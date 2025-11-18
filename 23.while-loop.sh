e82af21677fdc2594e4080cd0eff685b2336e5ab
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-11-18 15:20:02 +0530

    some message

--- 23.while-loop.sh
+++ 23.while-loop.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-echo "Starting Countdown..."
+# echo "Starting Countdown..."
 
-while [ $count -gt 0 ]
+# while [ $count -gt 0 ]
+# do
+#     echo "Time left: $count"
+#     sleep 1 # pause for 1 sec
+#     count=$((count - 1)) # Decrement the count
+# done
+
+# echo "Times Up!"
+
+while IFS= read -r line; 
 do
-    echo "Time left: $count"
-    sleep 1 # pause for 1 sec
-    count=$((count - 1)) # Decrement the count
-done
-
-echo "Times Up!"
+    # Process each line here
+    echo "Line: $line"
+    # You can perform other operations with the "$line" variable
+    # For example, you could parse data, perform calculations, etc.
+done < "20.script-1.sh"
