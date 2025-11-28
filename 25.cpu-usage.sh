1220c9d1bb69a0411f162d5266e68b340ec5b4e0
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-11-28 15:42:35 +0530

    shell-script

--- 25.cpu-usage.sh
+++ 25.cpu-usage.sh
@@ -2,8 +2,13 @@
 
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # in project we keep it as 75 
 
 while IFS= read -r line
 do
-    echo "Line: $line"
+    USAGE=$(echo $line | awk '{print $6}' | cut -d '%' -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ];then
+        echo "Higher Usage on $PARTITION: $USAGE"
+    fi
 done <<< $DISK_USAGE
