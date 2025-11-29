81d5224b6659e41feaf10fc2de7d72672d2669f5
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-11-29 15:33:29 +0530

    shell-script

--- 25.disk-usage.sh
+++ 25.disk-usage.sh
@@ -1,9 +1,9 @@
-#!bin/bash
+#!/bin/bash
 
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 # in project we keep it as 75 
-IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4) # -s indicates silent
 MESSAGE=""
 
 while IFS= read -r line
@@ -16,3 +16,10 @@ do
 done <<< $DISK_USAGE
 
 echo -e "Message Body: $MESSAGE"
+sh mail.sh "devopscloud0325@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "IP_ADDRESS" "DevOps Team"
+
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
