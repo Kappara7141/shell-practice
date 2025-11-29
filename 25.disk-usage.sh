2381f8c0d467f850d4496ba9fcc4f56be8c579ce
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-11-29 16:15:25 +0530

    shell-script

--- 25.disk-usage.sh
+++ 25.disk-usage.sh
@@ -16,7 +16,7 @@ do
 done <<< $DISK_USAGE
 
 echo -e "Message Body: $MESSAGE"
-sh mail.sh "devopscloud0325@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "IP_ADDRESS" "DevOps Team"
+sh mail.sh "kappara.siva@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "IP_ADDRESS" "DevOps Team"
 
 # TO_ADDRESS=$1
 # SUBJECT=$2
