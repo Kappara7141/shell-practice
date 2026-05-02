b6344bb1d9f179b723e0db1470699a582647ea82
sravya <ksravya66@gmail.com>
2026-05-02 20:45:38 +0530

    some message

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,4 +7,5 @@ echo "current directory: $PWD"
 echo "who is running this file: $USER"
 echo "Home directory of the user: $HOME"
 echo "PID of this script: $$"
+sleep 50 &
 echo "PID of the last command in the background: $!"
