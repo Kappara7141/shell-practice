5adde5639242fc0a6a1afa95f9500b20972a85fb
sravya <ksravya66@gmail.com>
2026-05-04 22:37:32 +0530

    some message

--- 13-installation.sh
+++ 13-installation.sh
@@ -4,14 +4,26 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR: Please run this script with root privilege"
-    exit 1
+    exit 1 # failure is other than 0
 fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo "ERROR: Installing $2 is failure"
+    exit 1 
+    else
+        echo "Installing $2 is SUCCESS"
+    fi
+}
 
 dnf install mysql -y
+VALIDATE $? "MySQL"
 
-if [ $? -ne 0 ]; then
-    echo "ERROR: Installing MYSQL is failure"
-    exit 1
-else
-    echo "Installing MYSQL is SUCCESS"
-fi
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python-3 -y
+VALIDATE $? "python-3"
+
+
