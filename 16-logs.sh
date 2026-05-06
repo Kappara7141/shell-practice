b00279b227876800c2ad4f1ccc15caf5e26218cb
sravya <ksravya66@gmail.com>
2026-05-06 11:34:22 +0530

    some message

--- 16-logs.sh
+++ 16-logs.sh
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "MySQL"
 else
-    echo -e "MySQL already exist ... $Y SKIPPING $N" | tee -a $LOG_FILE
+    echo -e "MySQL already exist ... $Y SKIPPING $N" 
 fi
 
 dnf list installed Nginx &>>$LOG_FILE
@@ -41,14 +41,13 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Nginx"
 else
-    echo -e "Nginx already exist ... $Y SKIPPING $N" | tee -a $LOG_FILE
-fi  
+    echo -e "Nginx already exist ... $Y SKIPPING $N" 
 
 dnf list installed python3 &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     dnf install python3 -y &>>$LOG_FILE
 VALIDATE $? "python3"
 else
-    echo -e "Python3 already exist ... $Y SKIPPING $N" | tee -a $LOG_FILE
+    echo -e "Python3 already exist ... $Y SKIPPING $N" 
 fi
 
