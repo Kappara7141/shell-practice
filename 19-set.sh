69bab40e13c209034653875bbcf8672fc9871c70
sravya <ksravya66@gmail.com>
2026-06-28 15:22:09 +0530

    roboshop

--- 19-set.sh
+++ 19-set.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+set -e
+
+error(){
+    echo "There is an error in $LINENO, Command is: $BASH_COMMAND"
+}
+
+Trap error ERR
+
 echo "Hello..."
 echo "Before Error"
-dchsdjlhvk;ljsfh
+dchsdjlhvk;ljsfh # here shell understand there is an error and signal is ERR
 echo "After Error"
